10a4e56084a5fc9be16b8e206d6d44fb5698d743
karelmolina <edmolina4@gmail.com>
2018-11-23 12:59:08 -0600

    Chapter2 Solution

--- chapter2/quiz1/questions/question_1.rb
+++ chapter2/quiz1/questions/question_1.rb
@@ -1,2 +1,12 @@
 # Write a program that uses constants to define the numbers 1 to 10 as
 # numerals.
+ONE = 1
+TWO = 2
+THREE = 3
+FOUR = 4
+FIVE = 5
+SIX = 6 
+SEVEN = 7
+EIGTH = 8
+NINE = 9
+TEN = 10
--- chapter2/quiz1/questions/question_2.rb
+++ chapter2/quiz1/questions/question_2.rb
@@ -1,2 +1,5 @@
 # Write a program that multiplies `TWELVE` by `FOURTEEN` by `SIXTEEN`
 # and `puts`es the result.
+$suma = 12 * 14 * 16
+
+puts $suma
